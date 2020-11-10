33a57603437596aba1850018e4e2040359bfb358
Sarbjotm <44513277+Sarbjotm@users.noreply.github.com>
2020-11-09 21:26:28 -0800

    Add files via upload

--- santa.py
+++ santa.py
@@ -1,17 +1,40 @@
 import random 
+import config
+import smtplib
+
+namesAndEmails = { #NAME: #EMAIL, 
+
+    }
 
 listOfNames = [] #ENTER NAMES HERE
-listofNamesChoosen = []
+listOfNamesLeft = [] #ENTER NAMES HERE
 
-for i in range(len(listOfNames)):
+
+
+server =  smtplib.SMTP('smtp.gmail.com', 587)
+server.starttls()
+server.login(config.EMAIL_ADDRESS,config.PASSWORD)
+        
+
+for i in listOfNames:
     while True:
-        r = random.randint(0,len(listOfNames)-1)
-        if(listOfNames[r] == listOfNames[i] or listOfNames[r] in listofNamesChoosen):
+        giftee = random.choice(listOfNamesLeft)
+        if(giftee == i):
             continue
         else:
             break
-    print(f"{listOfNames[i]} will give a gift to {listOfNames[r]}")
-    listofNamesChoosen.append(listOfNames[r])
+
+    subject = "Testing Python Script"
+    body = "Hello " + i + "\n\nWe would like to thank you for partcipating in our first Secret Santa Event. \n\n" + "Before we tell you who your giftee will be, we'd like to remind you of a few rules \n" + " \
+        1. No troll gifts are allowed, if you do bring a troll gift, your secret santa gift will be given to your giftee. \n \
+        2. Stay within the set budget of $0 to $x \nNow what you were waiting for, you will be " + giftee +"'s Secret Santa! \n\nWe'll be giving out gifts virtually on Month Date Year @ Time"
+    message = 'Subject: {} \n\n{}'.format(subject,body)
+    server.sendmail(config.EMAIL_ADDRESS, namesAndEmails.get(i), message)
+    print("Sent email!")
+    listOfNamesLeft.remove(giftee)
+    
+
+server.quit()
     
     
 
